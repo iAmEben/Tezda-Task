b23d7d319a22b0374e0ab9c5a55cf234cf44f819
iAmEben <justiceebenezer4@gmail.com>
2025-07-22 09:49:40 +0100

    feat(ui): update authstate to get name, email and avatar

--- lib/models/auth_state.dart
+++ lib/models/auth_state.dart
@@ -2,6 +2,16 @@ class AuthState {
   final bool isAuthenticated;
   final String? token;
   final String? refreshToken;
+  final String? name;
+  final String? email;
+  final String? avatar;
 
-  AuthState({this.isAuthenticated = false, this.token, this.refreshToken});
+  AuthState({
+    this.isAuthenticated = false,
+    this.token,
+    this.refreshToken,
+    this.name,
+    this.email,
+    this.avatar,
+  });
 }
